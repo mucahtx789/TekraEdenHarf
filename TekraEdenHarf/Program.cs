7b1bb65de297972b67c8acc6fde8e5dcb993321d
mucahtx789 <mucahtx789@gmail.com>
2025-01-12 09:57:06 +0300

    Proje dosyası ekle.

---
+++ TekraEdenHarf/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace TekraEdenHarf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bir kelime: ");
+
+            string word = Console.ReadLine();
+
+            string maxLetter = word[0].ToString();
+            int max = 0;
+            int number = 0;
+
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                for (int b = 0; b < word.Length; b++)
+                {
+                    if (word[i] == word[b])
+                    {
+                        number++;
+                    }
+                }
+                if (number > max) { 
+                    max = number;
+                maxLetter=word[i].ToString();
+                }
+                number = 0;
+
+            }
+            Console.WriteLine("tekrar eden harf :"+maxLetter+" tekrar sayısı :"+max );
+        }
+    }
+}
